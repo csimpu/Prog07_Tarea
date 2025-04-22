6f843df07e5ddab9d33eb226e5b66baf2fb8bf76
Borja TheFirstOne <borja.pinero@gmail.com>
2025-04-22 23:52:40 +0200

    Creada constructor y getter de CuentaAhorro

--- src/main/java/com/damel/modelos/CuentaAhorro.java
+++ src/main/java/com/damel/modelos/CuentaAhorro.java
@@ -8,6 +8,25 @@ package com.damel.modelos;
  *
  * @author borja
  */
-public class CuentaAhorro {
+public class CuentaAhorro extends CuentaBancaria {
+    
+    private double interesAnual;
+    
+    public CuentaAhorro (String iban, Persona titular, double saldo, double interesAnual) {
+        super(iban, titular, saldo);
+        this.interesAnual = interesAnual;
+    }
+
+    public double getInteresAnual() {
+        return interesAnual;
+    }
+    
+    @Override
+    public String devolverInfoString(){
+        return "Cuenta Ahorro\nIBAN: " + iban
+                +"\nTitular: " + titular.getNombre() + " " + titular.getApellidos()
+                +"\nSaldo: " + saldo + " €"
+                +"\nTipo de interes anual: " + interesAnual + "%";
+    }
     
 }
