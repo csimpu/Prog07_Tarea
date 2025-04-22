43141ff5e11e9f0d73bf044a856bcd618b42dba4
Borja ThePortableOne <borja.pinero@gmail.com>
2025-04-22 20:16:09 +0200

    Inicializar repositorio

---
+++ src/main/java/com/damel/modelos/Banco.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.damel.modelos;
+
+/**
+ * Programación DAM Modalidad Virtual
+ *
+ * Curso 2024/2025
+ *
+ * @author Borja Piñero
+ */
+public class Banco {
+
+}
