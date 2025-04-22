ec5ce3b1eebc9853caff313036224b635febd257
Borja TheFirstOne <borja.pinero@gmail.com>
2025-04-22 23:53:25 +0200

    Creada constructor y getter de CuentaCorrienteEmpresa

--- src/main/java/com/damel/modelos/CuentaCorrienteEmpresa.java
+++ src/main/java/com/damel/modelos/CuentaCorrienteEmpresa.java
@@ -8,6 +8,39 @@ package com.damel.modelos;
  *
  * @author borja
  */
-public class CuentaCorrienteEmpresa {
+public class CuentaCorrienteEmpresa extends CuentaCorriente {
+    private double interesDescubierto;
+    private double maxDescubierto;
+    private double comisionDescubierto;
+    
+    public CuentaCorrienteEmpresa(String iban, Persona titular, double saldo, String entidadesAutorizadas, double interesDEscubierto, double maxDescubierto, double comisionDescubierto){
+        super(iban, titular, saldo, entidadesAutorizadas);
+        this.interesDescubierto = interesDescubierto;
+        this.maxDescubierto = maxDescubierto;
+        this.comisionDescubierto = comisionDescubierto;
+    }
+
+    public double getInteresDescubierto() {
+        return interesDescubierto;
+    }
+
+    public double getMaxDescubierto() {
+        return maxDescubierto;
+    }
+
+    public double getComisionDescubierto() {
+        return comisionDescubierto;
+    }
+    
+    @Override
+    public String devolverInfoString(){
+        return "Cuenta Corriente Personal\nIBAN: " + iban
+                +"\nTitular: " + titular.getNombre() + " " + titular.getApellidos()
+                +"\nSaldo: " + saldo + " €"
+                +"\nMaximo descubierto: " + maxDescubierto + " €"
+                +"\nInterés por descubierto: " + interesDescubierto + "%"
+                +"\nComision por descubieto: " + comisionDescubierto + " €"
+                +"\nEntidades Autorizadas: " + entidadesAutorizadas;
+    }
     
 }
