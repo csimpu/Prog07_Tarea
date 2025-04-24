1e5f97cf2f73d11577ad8519ed0d1aaeb8706143
Borja TheFirstOne <borja.pinero@gmail.com>
2025-04-24 02:06:02 +0200

    Añadido javadoc Modificado toString

--- src/main/java/com/damel/objetos/Persona.java
+++ src/main/java/com/damel/objetos/Persona.java
@@ -5,9 +5,9 @@
 package com.damel.objetos;
 
 /**
- * Programación DAM Modalidad Virtual
- *
- * Curso 2024/2025
+ * Clase que gestiona los titulares de las cuentas<br><br>
+ * 
+ * Programación DAM Modalidad Virtual - Curso 2024/2025
  *
  * @author Borja Piñero
  */
@@ -17,6 +17,13 @@ public class Persona {
             apellidos,
             dni;
 
+    /**
+     * Constructor de la clase
+     * 
+     * @param nombre El nombre del titular.
+     * @param apellidos Los apellidos del titular.
+     * @param dni El DNI del titular.
+     */
     public Persona(String nombre, String apellidos, String dni) {
 
         this.nombre = nombre;
@@ -25,33 +32,64 @@ public class Persona {
 
     }
 
+    /**
+     * Getter para Nombre
+     * @return devuelve el nombre del titular
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Setter para nombre
+     * @param nombre El nombre del titular
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Getter para apellidos
+     * @return devuelve los Apellidos del titular.
+     */
     public String getApellidos() {
         return apellidos;
     }
 
+    /**
+     * Setter para Apellidos
+     * @param apellidos Los apellidos del titular
+     */
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+    /**
+     * Getter para dni
+     * @return devuelve el DNI
+     */
     public String getDni() {
         return dni;
     }
 
+    /**
+     * Setter para dni
+     * @param dni El DNI del titular
+     */
     public void setDni(String dni) {
         this.dni = dni;
     }
 
+    /**
+     * Método toString de Persona
+     * @return devuelve una {@code String} con los datos del titular de la cuenta
+     */
     @Override
     public String toString() {
-        return "Persona{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni + '}';
+        return "Persona:" 
+                + "\nNombre: " + nombre 
+                + "\nApellidos: " + apellidos 
+                + "\nDNI:" + dni;
     }
 
 }
